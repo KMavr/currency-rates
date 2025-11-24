0e7c7e1c28654310cb2a2bab687b1153b9c3a5bd
Konstantinos Mavrikas <mavrikas.konstantinos@gmail.com>
2025-11-24 23:14:35 +0200

    Install MUI Datepicker. Implement DatePicker functionality.

---
+++ src/components/DateSelection/DateSelection.tsx
@@ -0,0 +1,34 @@
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs, { Dayjs } from 'dayjs';
+import useCurrencyRatesStore from '../../store/useCurrencyRatesStore';
+import { DATE_FORMAT_UI, MAX_DAYS_BACK } from '../../model/date.ts';
+
+function DateSelection() {
+  const { selectedDate, setDate } = useCurrencyRatesStore();
+
+  const today = dayjs();
+  const minDate = today.subtract(MAX_DAYS_BACK, 'day');
+
+  const handleDateChange = (newDate: Dayjs | null) => {
+    if (newDate) {
+      setDate(newDate.toDate());
+    }
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+        label="Select Date"
+        value={dayjs(selectedDate)}
+        onChange={handleDateChange}
+        format={DATE_FORMAT_UI}
+        minDate={minDate}
+        maxDate={today}
+      />
+    </LocalizationProvider>
+  );
+}
+
+export default DateSelection;
